d7cf7703ca15be1c743c2520fa3ee25f1721c29c
CodeAngel247 <emily.f.svensson@hotmail.com>
2016-06-14 18:39:07 -0500

    Main Window View Model added

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porcupine.Plugin
+{
+    public class MainWindowViewModel
+    {
+
+        private string windowTitle;
+        public string WindowTitle
+        {
+            get { return windowTitle; }
+            private set { windowTitle = value; }
+        }
+        private ISplashScreen splash;
+        protected void SplashScreen(ISplashScreen splash)
+        {
+            this.splash = splash;
+            splash.SetProgressState(true);
+            splash.Progress(15.0);
+            splash.Progress(25.0);
+
+
+        }
+        public MainWindowViewModel(IEnvironmentSettings settings)
+        {
+            WindowTitle = settings.ApplicationName;
+        }
+    }
+}
