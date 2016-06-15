238b0935c66bb3c91b94c2405472d655eb11ee48
CodeAngel247 <emily.f.svensson@hotmail.com>
2016-06-14 22:07:07 -0500

    Create CustomerService.cs

---
+++ CustomerService.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MellowYellow
+{
+    public class CustomerService
+    {
+
+        public CustomerService()
+        {
+
+            LoadCustomerCollection();
+        }
+
+        public void LoadCustomerCollection()
+        {
+
+        }
+    }
