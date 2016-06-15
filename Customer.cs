ffcf0c62a3168104fff542413d1f649244c5796a
CodeAngel247 <emily.f.svensson@hotmail.com>
2016-06-14 22:06:38 -0500

    Create Customer.cs

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MellowYellow
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string BookTitle { get; set; }
+        public DateTime DOB { get; set; }
+        public bool IsMVP { get; set; }
+
+
+    }
+}
